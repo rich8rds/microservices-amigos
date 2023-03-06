35374980277b134e95685b4883724abfb86d69da
rich8rds <rbukunmi8@gmail.com>
2023-03-06 14:30:36 +0100

    Deleted notification module

--- notification/src/main/java/com/richards/service/NotificationService.java
+++
@@ -1,8 +0,0 @@
-package com.richards.service;
-
-import com.richards.clients.notification.NotificationRequest;
-
-public interface NotificationService {
-
-    NotificationRequest sendNotification(NotificationRequest notificationRequest);
-}
